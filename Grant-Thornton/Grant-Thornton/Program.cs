d51f73accd597a6c7a8dd949ea2c4e552376a4f8
KarlBaecklund <karl.baecklund@piktiv.se>
2024-04-04 01:12:05 +0200

    how to call all the classes

---
+++ Grant-Thornton/Grant-Thornton/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grant_Thornton.Quizzes;
+
+namespace Grant_Thornton;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    { 
+        var fibonacci = new Fibonacci(); // OK
+            
+        var square = new Square(); // OK
+            
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        var bubbleSort = new BubbleSort(); //OK
+            
+        var palindrome = new Palindrome();
+        
+        Console.WriteLine(fibonacci.GetValueOfFibonacciNumber(5));
+    } 
+}
