7774bdde391adb80b0977c6f7161b8f2de9ff070
KarlBaecklund <karl.baecklund@piktiv.se>
2024-04-04 10:41:07 +0200

    added new tests

--- Grant-Thornton/Grant-Thornton.Tests/PalindromeTest.cs
+++ Grant-Thornton/Grant-Thornton.Tests/PalindromeTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net;
 using JetBrains.Annotations;
 using Xunit;
 
@@ -11,6 +13,27 @@ public class PalindromeTest(Palindrome palindrome): IClassFixture<Palindrome>
     public void should_return_true_palindrome()
     {
         Assert.Equal(true, palindrome.GetStatus("Kayak"));
+    }
+    
+    [Fact]
+    public async void should_return_true_401()
+    {
+        var respond = await palindrome.GetRespond("not_the_correct_query");
+        Assert.Equal(HttpStatusCode.Unauthorized, respond.StatusCode);
+    }
+    
+    [Fact]
+    public async void should_return_true_200()
+    {
+        var respond = await palindrome.GetRespond("DRuQSdrjDG_syswkTpRhz2l0wt_tDoOmFTGLhCCni_MDAzFuYF6Bkg==");
+        Assert.Equal(HttpStatusCode.OK  , respond.StatusCode);
+    }
+    
+    [Fact]
+    public async void should_return_thrown()
+    {
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => palindrome.GetPalindrome("not_the_correct_query"));
+        Assert.Equal("got response code: Unauthorized", exception.Message.ToString());
     }
     
     [Fact]
